1d15c96f31d378145c6ff045fc1d60a79967998c
greghorne <greghorne@hotmail.com>
2018-08-09 14:58:35 +0000

    updated map layer names and changed server polling time

--- js/tfd.js
+++ js/tfd.js
@@ -27,28 +27,28 @@ const CONST_BASE_LAYER              = 1
 // definition of map layers; first layer is the default layer displayed
 const CONST_MAP_LAYERS = [
     {
-        name: "Grayscale",
+        name: "Grayscale OSM",
         url: "http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png",
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
         minZoom:  5,
         maxZoom: 17
     },
     {
-        name: "Esri",
+        name: "Esri OSM",
         url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
         attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
         minZoom:  5,
         maxZoom: 17
     },
     {
-        name: "Hydda",
+        name: "Hydda OSM",
         url: "https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png",
         attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         minZoom:  5,
         maxZoom: 17
     },
     {
-        name: "Standard OSM",
+        name: "Basic OSM",
         url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
         minZoom:  5,
