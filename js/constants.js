b7c708e5641f58c2222f635c5307289cecf638a1
ghorne <greghorne@hotmail.com>
2022-04-05 19:14:09 -0500

    adjusted available maps, removed citygram icon

--- js/constants.js
+++ js/constants.js
@@ -39,10 +39,8 @@ const CONST_3D_BUILDINGS_URL            = 'https://{s}.data.osmbuildings.org/0.3
 // definition of map layers; first layer is the default layer displayed
 const CONST_MAP_LAYERS = [
     {
-        // not https so can generate warnings due to mixed content
-        // 2018-08-12 - https site currently has a NET::ERR_CERT_COMMON_NAME_INVALID
-        name: "Grayscale OSM",
-        url: "http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png",      
+        name: "Basic OSM",
+        url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
         minZoom:  5,
         maxZoom: 17
@@ -51,20 +49,6 @@ const CONST_MAP_LAYERS = [
         name: "Esri OSM",
         url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
         attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
-        minZoom:  5,
-        maxZoom: 17
-    },
-    {
-        name: "Hydda OSM",
-        url: "https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png",
-        attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        minZoom:  5,
-        maxZoom: 17
-    },
-    {
-        name: "Basic OSM",
-        url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
         minZoom:  5,
         maxZoom: 17
     },
